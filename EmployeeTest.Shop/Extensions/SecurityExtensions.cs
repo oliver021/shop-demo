ef3b03b869e719c2105f66d29ce7c29765988615
oliver021 <oliver021val@gmail.com>
2021-03-12 23:31:07 -0500

    Add security services

--- EmployeeTest.Shop/Extensions/SecurityExtensions.cs
+++ EmployeeTest.Shop/Extensions/SecurityExtensions.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -25,15 +27,45 @@ namespace Microsoft.Extensions.DependencyInjection
                 throw new ArgumentNullException(nameof(configuration));
             }
 
+            // add identity service
             services.AddIdentity<User, IdentityRole<ulong>>()
                 .AddEntityFrameworkStores<ShopContext>()
-                .AddSignInManager()
                 .AddDefaultTokenProviders();
 
+            // low requirements for security options
+            services.Configure<IdentityOptions>(options => {        
+                options.Password.RequireDigit = false;       
+                options.Password.RequireLowercase = false;   
+                options.Password.RequireNonAlphanumeric = false; 
+                options.Password.RequireUppercase = false;      
+                options.Password.RequiredLength = 6;        
+                options.Password.RequiredUniqueChars = 1;
+                                                                    
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(5); 
+                options.Lockout.MaxFailedAccessAttempts = 5;       
+                options.Lockout.AllowedForNewUsers = true;
+                                                                   
+                options.User.RequireUniqueEmail = false;    
+            });
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme,
-                options => configuration.Bind("JwtSecurity", options));
+             // add auth service
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                    options.SaveToken = true;
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSecurity:Secret"]))
+                    };
+            });
 
             return services;
         }
