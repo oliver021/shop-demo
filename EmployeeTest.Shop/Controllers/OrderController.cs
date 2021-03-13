ee2876b8df24b125c586bc8549106aaf36b1b825
oliver021 <oliver021val@gmail.com>
2021-03-13 02:30:01 -0500

    Ad order controller

--- EmployeeTest.Shop/Controllers/OrderController.cs
+++ EmployeeTest.Shop/Controllers/OrderController.cs
@@ -1,16 +1,40 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OliDemos.Shop.Model;
+using OliDemos.Shop.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OliDemos.Shop.Controllers
 {
     [ApiController]
-    [Authorize("Admin")]
+    [Authorize(Roles = "Admin")]
     [Route("/api/[controller]")]
     public class OrderController : Controller
     {
+        private readonly OrderService service;
+
+        public OrderController(OrderService service)
+        {
+            this.service = service;
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrder([FromBody] OrderUpdate update)
+        {
+            await service.UpdateAsync(update);
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] uint order)
+        {
+            await service.DeleteAsync(order);
+            return NoContent();
+        }
     }
 }
