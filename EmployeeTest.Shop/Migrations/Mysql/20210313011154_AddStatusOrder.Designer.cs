19348fb92fdc8cebcd43ec1bbae5c246ea775b29
oliver021 <oliver021val@gmail.com>
2021-03-12 23:30:35 -0500

    add migration to set relationship

--- EmployeeTest.Shop/Migrations/Mysql/20210313011154_AddStatusOrder.Designer.cs
+++ EmployeeTest.Shop/Migrations/Mysql/20210313011154_AddStatusOrder.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OliDemos.Shop.Services;
 
-namespace DemoShop.Migrations.Migrations.Mysql
+namespace OliDemos.Shop.Migrations.Mysql
 {
     [DbContext(typeof(ShopContext))]
-    partial class ShopContextModelSnapshot : ModelSnapshot
+    [Migration("20210313011154_AddStatusOrder")]
+    partial class AddStatusOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EmployeeTest.Shop/Migrations/Mysql/20210313011154_AddStatusOrder.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OliDemos.Shop.Migrations.Mysql
+{
+    public partial class AddStatusOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Order",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<ulong>(
+                name: "UserId",
+                table: "Order",
+                type: "bigint unsigned",
+                nullable: false,
+                defaultValue: 0ul);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(ulong),
+                oldType: "bigint unsigned");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_UserId",
+                table: "Order",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_AspNetUsers_UserId",
+                table: "Order",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Order_AspNetUsers_UserId",
+                table: "Order");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Order_UserId",
+                table: "Order");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Order");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Order");
+
+            migrationBuilder.AlterColumn<ulong>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "bigint unsigned",
+                nullable: false,
+                defaultValue: 0ul,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+        }
+    }
+}
